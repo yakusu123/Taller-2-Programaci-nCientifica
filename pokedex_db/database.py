7c01b06bd5de27f9e9117a2d7f199d9f3abb27f9
yakusu123 <vic3nt3.ord3n3s@gmail.com>
2026-06-03 22:48:09 -0400

    feat: add Database code, with create_db and get_session

---
+++ pokedex_db/database.py
@@ -0,0 +1,13 @@
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = "sqlite:///pokedex.db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
