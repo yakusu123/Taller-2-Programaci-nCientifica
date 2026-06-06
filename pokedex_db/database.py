2e0e8da5c9d2d62b86fbf2744048e44753b5b072
srCochayuyo <129439145+srCochayuyo@users.noreply.github.com>
2026-06-06 04:10:05 -0400

    Refactor: clean code and formatting improvements

--- pokedex_db/database.py
+++ pokedex_db/database.py
@@ -1,13 +1,14 @@
+from collections.abc import Generator
 from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///pokedex.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
 
-def create_db():
+def create_db() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-def get_session():
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
--- pokedex_db/main.py
+++ pokedex_db/main.py
@@ -4,16 +4,17 @@ from seed import run_seed
 
 def main() -> None:
     """
-    Programa principal.
-    Crea base de datos, sus tablas y pobla con datos iniciales
+    Crear base de datos y poblarla con datos iniciales
     """
 
-    print("----------- Iniciando ----------- ")
-    print("\n----------- Levantamiento de base de datos y tablas ----------- ")
+    print("----------- Iniciando -----------")
+    print("\n----------- Levantamiento de base de datos y tablas -----------")
     create_db()
-    print("\n ----------- Poblando Base de datos ----------- ")
+
+    print("\n ----------- Poblando Base de datos -----------")
     run_seed()
-    print("\n----------- Finalizado con exito! ----------- ")
+
+    print("\n----------- Finalizado con exito! -----------")
 
 
 if __name__ == "__main__":
