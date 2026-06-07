8201a1fe8a0e2d2b23de6b49ac83df1dd36109f7
yakusu123 <vic3nt3.ord3n3s@gmail.com>
2026-06-07 18:10:06 -0400

    Feat: add querie for consulta_libre

--- pokedex_db/queries.py
+++ pokedex_db/queries.py
@@ -1,23 +1,33 @@
 from sqlmodel import Session, select, func, col, case
 from models import Pokemon, Entrenador, Tipo, PokemonTipo, Participacion, Batalla
+from pokedex_db.models import Region
+from sqlalchemy import text
 
-#
-# def pokemon_alto_nivel(session: Session, umbral: int = 70) -> list[Pokemon]:
-#    """
-#    Retorna los Pokémon con nivel mayor o igual al umbral, de mayor a menor nivel.
-#    """
-#    cumple = session.query(Pokemon).where(Pokemon.nivel >= umbral).order_by(Pokemon.nivel.asc())
-#    resultado = session.execute(cumple).all()
-#    return resultado
-#
-#
-# def campeones_por_region(session: Session, nombre_region: str) -> list[Entrenador]:
-#    """
-#    Retorna a los entrenadores campeones por una región especifica
-#    """
-#    cumple = session.query(Entrenador).where(Entrenador.es_campeon == True).where(Entrenador.nombre_region == nombre_region)
-#    resultado = session.execute(cumple).all()
-#    return resultado
+
+def pokemon_alto_nivel(session: Session, umbral: int = 70) -> list[Pokemon]:
+    """
+    Retorna los Pokémon con nivel mayor o igual al umbral, de mayor a menor nivel.
+    """
+    consulta = (
+        select(Pokemon.nombre, Pokemon.nivel, Pokemon.apodo)
+        .where(Pokemon.nivel >= umbral)
+        .group_by(Pokemon.nivel.desc())
+    )
+    resultado = session.exec(consulta).all()
+    return [(r[0], r[1], r[2]) for r in resultado]
+
+
+def campeones_por_region(session: Session, nombre_region: str) -> list[Entrenador]:
+    """
+    Retorna a los entrenadores campeones por una región especifica
+    """
+    consulta = (
+        select(Entrenador.id, Entrenador.nombre, Entrenador.region)
+        .where(Entrenador.es_campeon == True)
+        .where(Entrenador.region == nombre_region)
+    )
+    resultado = session.exec(consulta).all()
+    return [(r[0], r[1], r[2]) for r in resultado]
 
 
 def shiny_con_apodo(session: Session) -> list[tuple[str, str, int, str]]:
@@ -78,7 +88,7 @@ def estadisticas_batallas(session: Session) -> list[tuple[str, int, int, int]]:
             func.count(col(Participacion.batalla_id)).label("total"),
             func.sum(
                 case((col(Batalla.ganador_id) == col(Entrenador.id), 1), else_=0)
-            ).label("victorias")
+            ).label("victorias"),
         )
         .join(Participacion, col(Entrenador.id) == col(Participacion.entrenador_id))
         .join(Batalla, col(Participacion.batalla_id) == col(Batalla.id))
@@ -89,3 +99,32 @@ def estadisticas_batallas(session: Session) -> list[tuple[str, int, int, int]]:
     resultados = session.exec(consulta).all()
 
     return [(r[0], r[1], int(r[2] or 0), r[1] - int(r[2] or 0)) for r in resultados]
+
+
+def region_mas_insignias(session: Session) -> tuple[str, float]:
+    """
+    Retorna la region con un promedio mayor de insignias.
+    """
+    consulta = (
+        select(Region.nombre, func.round(func.avg(Entrenador.insignias), 2))
+        .join(Entrenador)
+        .group_by(col(Region.nombre))
+        .order_by(func.avg(Entrenador.insignias).asc())
+    )
+    resultados = session.exec(consulta).first()
+    return [(r[0], r[1]) for r in resultados]
+
+
+def consulta_libre(session: Session) -> tuple[dict]:
+    """
+    retorna el porcentaje de pokemons de tipo agua que han sido parte de un equipo ganador
+    """
+    consulta = text("""SELECT count(p.nombre)
+                    FROM Batallas b
+                    INNER JOIN Entrenador e ON e.id = b.ganador_id
+                    INNER JOIN Pokemon p ON p.entrenador_id = e.id
+                    INNER JOIN PokemonTipo pt ON pt.pokemon_id = p.id
+                    INNER JOIN Tipo t ON t.id = pt.tipo_id
+                    WHERE t.nombre = 'Agua' """)
+    resultados = session.exec(consulta).all()
+    return [dict(r) for r in resultados]
